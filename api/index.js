79bb591244c1c6fb7b2ce3ef759b06acf2203291
Petar Cevriz <pcevriz95@gmail.com>
2024-05-27 18:08:56 +0200

    Add api git ignore file, secret example and steps on how we are going to write jwt

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(userRoutes);
 
+// Setup server to accept headers
+// Install JWT and setup middleware
+// Use middleware on getUsers route
+
 app.listen("3000", () => {
   console.log("Port listening on port 3000");
 });
